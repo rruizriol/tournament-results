91b97376cb0f3f055e96dc1d1139d8900b33736d
rruizriol <rembrandtruizriol@hotmail.com>
2015-03-24 00:47:20 -0400

    Committed by Udacity

---
+++ AppCode/tournament.sql
@@ -0,0 +1,37 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-- Create Database
+--CREATE DATABASE tournament;
+
+--Create Tables
+CREATE TABLE players (
+    id        serial CONSTRAINT players_key PRIMARY KEY,
+    name      varchar(50) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE matches(
+    id        serial CONSTRAINT matches_key PRIMARY KEY,
+    winner    integer NOT NULL DEFAULT 0
+);
+
+CREATE TABLE match_players (
+    id        serial CONSTRAINT match_players_key PRIMARY KEY,
+    match_id   integer NOT NULL DEFAULT 0,
+    player_id  integer  NOT NULL DEFAULT 0,
+    score     real  NOT NULL DEFAULT 0
+);
+
+--Create Views
+CREATE VIEW player_standings AS 
+  SELECT id
+        ,name
+        ,(select count(id) from matches where winner = players.id) as wins
+        ,(select count(id) from match_players where player_id = players.id) as matches
+        FROM players;
+
